26419383b6375614138e18dd0205e8b82c9e0fbd
Maxim Kuznetsov <max.kuznetsov@yahoo.com>
2020-11-12 15:53:12 +0300

    add initial project files

---
+++ Lee algorithm.cpp
@@ -0,0 +1,12 @@
+﻿#include "lee.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	Lee task = Lee(10, 5);
+	auto res = task.Findpath({ 7, 0 }, { 5, 4 });
+	cout << res.second << endl;
+	system("pause");
+	return 0;
+}
