6c0dac48a8bf01fe7cb18954ab93b5c626f2e794
MaxKuznets0v <max.kuznetsov@yahoo.com>
2020-11-15 21:40:40 +0300

    add results saving

--- Lee test.cpp
+++ Lee test.cpp
@@ -1,19 +1,51 @@
 ﻿#include "lee.h"
 #include <iostream>
+#include <fstream>
+#include <vector>
+
 using namespace std;
 
 int main()
 {
-	Lee test;
-	int testNum, width, length;
-	width = 10;
-	length = 15;
-	testNum = 10;
-	vector<int> res = test.GenerateTask(width, length, testNum);
+	ifstream results("results.txt");
+	ofstream frequency("frequency.txt", ios_base::trunc);
+	vector < double > res;
+	double num;
+	while (results >> num)
+		res.push_back(num);
+
+
+	int interval = 50;
+	double min = 0, max = 1;
+	double step = 1.0 * (max - min) / interval;
+	cout << step << endl;
+	vector<int> count(interval);
+	for (auto& r : res)
+	{
+		int ratio = (r - min) / step;
+		if (ratio != interval)
+			count[ratio]++;
+		else
+			count[ratio - 1]++;
+	}
+	for (int i = 0; i < count.size(); ++i)
+	{
+		frequency << 1. * count[i] / res.size() << '\n';
+	}
+	frequency << "---\n";
+	for (int i = 0; i < count.size(); ++i)
+		frequency << i * step << '\n';
+
+	/*Lee test;
+	int width = 80;
+	int length = 90;
+	int testNum = 10000;
+	ofstream results("results.txt", ios_base::trunc);
+
+	vector<int> res = test.GenerateTask(width, length, testNum, true);
 
 	for (auto& elem : res)
-		cout << elem << " ";
-
-	cout << endl;
+		results << elem << "\n";*/
+	results.close();
 	return 0;
 }
